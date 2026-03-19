0131231058219d349e7ac5c2bcb036bc18fec450
kwl <12430774+kwl11429081_admin@user.noreply.gitee.com>
2026-03-19 16:42:46 +0800

    Fix render.yaml to use Docker for Python service

--- python_vectorizer/app.py
+++ python_vectorizer/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sentence_transformers import SentenceTransformer
@@ -31,4 +32,5 @@ def embed():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
